66b52fa99102e660fe60a2c6b08c94c53bff272f
Lvl3Mage <lvlthreemage@gmail.com>
2022-10-27 18:11:08 +0200

    Added tiles and movement to char

---
+++ Assets/CamFollower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollower : MonoBehaviour
+{
+    [SerializeField] Transform followedObject;
+    [SerializeField] float lerpSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = Vector2.Lerp(transform.position,followedObject.position, lerpSpeed * Time.deltaTime);
+        transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
+    }
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     [SerializeField] float Speed;
+    [SerializeField] float lerpSpeed;
+    Vector2 targetPos;
     // Start is called before the first frame update
     void Start()
     {
-
+        targetPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + Speed;
-        transform.position = position;
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+        targetPos += new Vector2(hAxis, vAxis) * Speed * Time.deltaTime;
+
+        transform.position = Vector2.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
     }
 }
