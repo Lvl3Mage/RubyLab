7a530d475759bd9ef69287f521ce08f7528551d2
Lvl3Mage <lvlthreemage@gmail.com>
2022-11-17 18:48:02 +0100

    Added destroyed flag

--- Assets/DestructableEnemy.cs
+++ Assets/DestructableEnemy.cs
@@ -6,8 +6,13 @@ public class DestructableEnemy : MonoBehaviour
 {
     [SerializeField] Object destructionEffect;
     [SerializeField] EnemyController controller;
+    bool destroyed = false;
     [ContextMenu("DestroyEnemy")]
     public void Damage(){
+        if(destroyed){
+            return;
+        }
+        destroyed = true;
         Instantiate(destructionEffect, transform.position, transform.rotation);
         controller.Stop();
         // gameObject.SetActive(false);
